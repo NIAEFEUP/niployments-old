208fa3b79138b4932f0da8065e364ef81e479190
miguelpduarte <miguelpduarte98@gmail.com>
2019-09-01 17:49:23 +0100

    Update niployments slack channel name

--- slack/messaging.sh
+++ slack/messaging.sh
@@ -10,20 +10,22 @@ set -ueo pipefail
 curr_dir="${BASH_SOURCE%/*}"
 if [[ ! -d "$curr_dir" ]]; then curr_dir="${0%/*}"; fi
 
+NIPLOYMENTS_CHANNEL='#niployments'
+
 # Should work with any files up to 1MB, as per slack API: https://api.slack.com/methods/files.upload
 function send_file_message() {
     # filepath should be absolute to ensure there are no problems in reading it
     local message="$1" file_path="$2"
 
     # stderr to stdout and then using jq to pretty-print it
-    # filename is calculated by removing the leading directories from file_path
-    # using content instead of passing the file so that there is a preview
+    # The filename is calculated by removing the leading directories from file_path
+    # Using content instead of passing the file so that there is a preview
     exec 2>&1
     curl --silent \
     -F "filename=${file_path##*/}" \
     -F "content=$(cat "$file_path")" \
     -F "initial_comment=$message" \
-    -F channels='#github' \
+    -F channels="$NIPLOYMENTS_CHANNEL" \
     -H "Authorization: Bearer $(cat "$curr_dir/niployments-bot.slack.token")" \
     https://slack.com/api/files.upload \
     | jq .
