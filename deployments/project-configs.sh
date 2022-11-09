0747a058e50149121680ad0011cb0420ae3971cd
Ubuntu <ni@aefeup.pt>
2022-11-09 17:14:10 +0000

    Do not hardcode home path

--- deployments/project-configs.sh
+++ deployments/project-configs.sh
@@ -15,30 +15,30 @@ declare -A project_docker_flags
 
 # tts
 project_port[tts-fe---master]=3100
-project_dotenv_location[tts-fe---master]='/home/ni/niployments/deployments/env-files/nijobs-be/master/.env'
+project_dotenv_location[tts-fe---master]='~/niployments/deployments/env-files/nijobs-be/master/.env'
 
 # (Thanks to this modular config, it is possible to also deploy staging (painlessly!))
 # nijobs-fe
 project_port[nijobs-fe---master]=4001
-project_dotenv_location[nijobs-fe---master]='/home/ni/niployments/deployments/env-files/nijobs-fe/master/.env'
+project_dotenv_location[nijobs-fe---master]='~/niployments/deployments/env-files/nijobs-fe/master/.env'
 
 ## nijobs-fe staging
 project_port[nijobs-fe---develop]=4002
-project_dotenv_location[nijobs-fe---develop]='/home/ni/niployments/deployments/env-files/nijobs-fe/develop/.env'
+project_dotenv_location[nijobs-fe---develop]='~/niployments/deployments/env-files/nijobs-fe/develop/.env'
 
 ## nijobs-fe experimental (pre-develop, "true staging" vs staging=beta/nightly)
 project_port[nijobs-fe---experimental]=4003
-project_dotenv_location[nijobs-fe---experimental]='/home/ni/niployments/deployments/env-files/nijobs-fe/experimental/.env'
+project_dotenv_location[nijobs-fe---experimental]='~/niployments/deployments/env-files/nijobs-fe/experimental/.env'
 
 # nijobs-be
 project_port[nijobs-be---master]=4010
-project_dotenv_location[nijobs-be---master]='/home/ni/niployments/deployments/env-files/nijobs-be/master/.env.local'
-project_docker_flags[nijobs-be---master]='-v /home/ni/niployments/deployments/volumes-data/nijobs:/usr/src/app/static'
+project_dotenv_location[nijobs-be---master]='~/niployments/deployments/env-files/nijobs-be/master/.env.local'
+project_docker_flags[nijobs-be---master]='-v ~/niployments/deployments/volumes-data/nijobs:/usr/src/app/static'
 
 ## nijobs-be staging
 project_port[nijobs-be---develop]=4011
-project_dotenv_location[nijobs-be---develop]='/home/ni/niployments/deployments/env-files/nijobs-be/develop/.env.local'
-project_docker_flags[nijobs-be---develop]='-v /home/ni/niployments/deployments/volumes-data/nijobs-beta:/usr/src/app/static'
+project_dotenv_location[nijobs-be---develop]='~/niployments/deployments/env-files/nijobs-be/develop/.env.local'
+project_docker_flags[nijobs-be---develop]='-v ~/niployments/deployments/volumes-data/nijobs-beta:/usr/src/app/static'
 # debug example:
 # project_dotenv_location[nijobs-be---develop]='/home/miguel/Coding/NIAEFEUP/niployments/deployments/env-files/nijobs-be/develop/.env.local'
 
